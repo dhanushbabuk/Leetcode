9103dd1ca0749e6d20c9598999f73c91110b281e
Dhanush Babu K <103583233+dhanushbabuk@users.noreply.github.com>
2024-04-01 02:34:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (84.42%) - LeetSync

--- Medium/1970-sorting-the-sentence/sorting-the-sentence.cpp
+++ Medium/1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -8,9 +8,9 @@ public:
         int i = 0;
         int count=0;
        
-        while(i < s.size())
+        while(i < s.size() || temp.size()!=0)
         {
-            if(s[i]==' ')
+            if(s[i]==' '|| i==s.size())
             {
                 int pos = temp[temp.size()-1] - '0';
                     temp.pop_back();
@@ -19,18 +19,18 @@ public:
                     count++;
             }   
             else
-                temp+=s[i];
-                
+             {
+                   temp+=s[i];
+             }
+               
                 i++;
-
-            
         }
 
-                 int pos = temp[temp.size()-1] - '0';
-                    temp.pop_back();
-                    ans[pos]=temp;
-                    temp.clear();
-                    count++;
+                //  int pos = temp[temp.size()-1] - '0';
+                //     temp.pop_back();
+                //     ans[pos]=temp;
+                //     temp.clear();
+                //     count++;
                     
               for(int i=1;i<=count;i++)
               {
