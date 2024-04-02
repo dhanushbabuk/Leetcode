336246b7934115c557c47460bb1894435a3944e7
Dhanush Babu K <103583233+dhanushbabuk@users.noreply.github.com>
2024-04-02 22:01:58 +0530

    Time: 6 ms (65.28%) | Memory: 9.2 MB (63.23%) - LeetSync

--- Medium/205-isomorphic-strings/isomorphic-strings.cpp
+++ Medium/205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,11 +1,8 @@
-#include <string>
-#include <unordered_map>
-
 class Solution {
 public:
     bool isIsomorphic(std::string s, std::string t) {
-        std::unordered_map<char, char> s_to_t;
-        std::unordered_map<char, char> t_to_s;
+        unordered_map<char, char> s_to_t;
+        unordered_map<char, char> t_to_s;
 
         for (int i = 0; i < s.size(); ++i) {
             char s_char = s[i];
@@ -13,6 +10,7 @@ public:
 
             // Check mapping from s to t
             if (s_to_t.find(s_char) == s_to_t.end()) {
+                // not found
                 s_to_t[s_char] = t_char;
             } else {
                 if (s_to_t[s_char] != t_char) {
@@ -22,6 +20,7 @@ public:
 
             // Check mapping from t to s
             if (t_to_s.find(t_char) == t_to_s.end()) {
+                // not found
                 t_to_s[t_char] = s_char;
             } else {
                 if (t_to_s[t_char] != s_char) {
