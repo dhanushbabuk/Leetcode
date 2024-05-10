194c943add27cdac0ab94c9223583dcc47bfe3f7
Dhanush Babu K <103583233+dhanushbabuk@users.noreply.github.com>
2024-05-10 22:40:14 +0530

    Time: 3 ms (48.05%) | Memory: 15.5 MB (5.40%) - LeetSync

--- Medium/78-subsets/subsets.cpp
+++ Medium/78-subsets/subsets.cpp
@@ -10,26 +10,21 @@ private:
             return;
         }
         
-  
-        
         vector<int> op1 = op;
-        vector<int> op2 = op ;
+        vector<int> op2 = op;
+
         op2.push_back(ip[0]);         
-          
-        
         ip.erase(ip.begin()+0);          
         
-        
         generate(ip,op2);
-        
         generate(ip,op1);
         
     }
+    
 public:
     vector<vector<int>>subsets(vector<int>& nums) {
     
          vector<int>op;
-    
          generate(nums,op);
 
         return ans;  
