41197bb0fca759ae83d17168c30fead6388340b1
Dhanush Babu K <103583233+dhanushbabuk@users.noreply.github.com>
2024-06-05 01:08:17 +0530

    Time: 22 ms (59.74%) | Memory: 31 MB (85.36%) - LeetSync

--- Medium/966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ Medium/966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
     public :
 
-    int fun(vector<int> &nums,int goal){
-        int l=0;
-        int r=0;
-        int sum=0;
-        int cnt=0;
-        if(goal<0){
-            return 0;
-        }
-        while(r<nums.size()){
-            sum += nums[r];
-            while(sum>goal){
-                if(nums[l]==1){
-                    
-                    sum -= nums[l];
-                }
-                l++;
+  int fun(std::vector<int>& nums, int k) {
+        if (k < 0) return 0;
 
+        int l = 0, r = 0, cnt = 0, sum = 0;
+
+        while (r < nums.size()) {
+            sum += nums[r] % 2; 
+
+            while (sum > k) { 
+                sum -= nums[l] % 2; 
+                l++;
             }
-            cnt=cnt+r-l+1;
-            r++;
+
+            cnt += (r - l + 1); 
+            r++; 
         }
+
         return cnt;
     }
 
