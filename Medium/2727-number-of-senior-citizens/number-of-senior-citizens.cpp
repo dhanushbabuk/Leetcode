afb00653c336cf4843d7120cae786f7e0a898953
Dhanush Babu K <103583233+dhanushbabuk@users.noreply.github.com>
2024-08-08 16:02:45 +0530

    Time: 8 ms (48.63%) | Memory: 17.6 MB (47.98%) - LeetSync

---
+++ Medium/2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        
+        int cnt = 0;
+        for(string detail : details)
+        {
+            if(detail[11] > '6' || (detail[11] =='6' && detail[12] > '0') )
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
